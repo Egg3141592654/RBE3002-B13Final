6a6f725c54f5a2b621a0b30aab4f40651760c6f7
egg3141592654 <egg3141592654@gmail.com>
2013-12-16 00:34:33 -0500

    Restructure to catkinize the repo from root

---
+++ gitRBE/build/finalProject/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eric/Desktop/gitRBE/src/finalProject
+# Build directory: /home/eric/Desktop/gitRBE/build/finalProject
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
